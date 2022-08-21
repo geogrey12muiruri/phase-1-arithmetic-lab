ccecbdc1ff7aef3b8912b667124e99966f2f073c
geogrey12muiruri <greggambugua@gmail.com>
2022-08-21 05:36:31 +0300

    completed the assaignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 20;
+const mod = num3 % num4;
+const max = Math.max(10, 20,5,3,2);
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+random.isInteger((random));
 
-// Write your code here
+
+
